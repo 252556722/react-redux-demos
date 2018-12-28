310fcb3ca49511cfb6e6faf1e8fa2d4c8033ac61
2190907815@qq.com <2190907815@qq.com>
2018-12-28 17:17:40 +0800

    make-comment1

---
+++ make-comment1/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import CommentApp from './CommentApp'
+import './index.css'
+import PropTypes from 'prop-types'
+ReactDOM.render(
+  <CommentApp />,
+  document.getElementById('root')
+);
